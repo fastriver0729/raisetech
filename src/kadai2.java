5a6efac6a595e77423f2a97d411535db80adb41c
th072 <th072@LAPTOP-O8EEQ7H5>
2020-06-15 20:45:31 +0900

    raisetech

---
+++ src/kadai2.java
@@ -0,0 +1,18 @@
+
+public class kadai2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		String greeting="Hello world!";
+		
+		System.out.println(greeting);
+
+		int sum=0;
+		for(int i=1; i<=10; i++) {
+		sum+=i;
+		}
+	    System.out.println(sum);	
+	}
+
+}
